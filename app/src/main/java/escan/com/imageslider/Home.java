68e058a8cee9713457cd83a5288e9a41ecc63988
Rahul chaube <rahulrkumar7@gmail.com>
2018-03-17 11:55:13 +0530

    Initial commit

---
+++ app/src/main/java/escan/com/imageslider/Home.java
@@ -0,0 +1,66 @@
+package escan.com.imageslider;
+
+import android.app.ActionBar;
+import android.os.Handler;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import java.util.ArrayList;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import me.relex.circleindicator.CircleIndicator;
+
+public class Home extends AppCompatActivity {
+    private static ViewPager mPager;
+    private static int currentPage = 0;
+    private static final Integer[] XMEN= {R.drawable.captain_america,
+            R.drawable.superman,
+            R.drawable.got,R.drawable.thor,R.drawable.superman,
+            R.drawable.iron_man,R.drawable.thor,R.drawable.wolvrine};
+    private ArrayList<Integer> XMENArray = new ArrayList<Integer>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        View decorView = getWindow().getDecorView();
+// Hide the status bar.
+        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
+// Remember that you should never show the action bar if the
+// status bar is hidden, so hide that too if necessary.
+//        ActionBar actionBar = getActionBar();
+//        actionBar.hide();
+        init();
+    }
+    private void init() {
+        for(int i=0;i<XMEN.length;i++)
+            XMENArray.add(XMEN[i]);
+
+        mPager = (ViewPager) findViewById(R.id.pager);
+        mPager.setAdapter(new MyAdapter(Home.this,XMENArray));
+        CircleIndicator indicator = (CircleIndicator) findViewById(R.id.indicator);
+        indicator.setViewPager(mPager);
+
+        // Auto start of viewpager
+        final Handler handler = new Handler();
+        final Runnable Update = new Runnable() {
+            public void run() {
+                if (currentPage == XMEN.length) {
+                    currentPage = 0;
+                }
+                mPager.setCurrentItem(currentPage++, true);
+            }
+        };
+        Timer swipeTimer = new Timer();
+        swipeTimer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+//                handler.post(Update);
+            }
+        }, 2500, 2500);
+    }
+}
---
+++ app/src/main/java/escan/com/imageslider/MyAdapter.java
@@ -0,0 +1,51 @@
+package escan.com.imageslider;
+
+import android.content.Context;
+import android.support.v4.view.PagerAdapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Android7 on 3/16/2018.
+ */
+
+public class MyAdapter extends PagerAdapter {
+
+    private ArrayList<Integer> images;
+    private LayoutInflater inflater;
+    private Context context;
+    public MyAdapter(Context context, ArrayList<Integer> images) {
+        this.context = context;
+        this.images=images;
+        inflater = LayoutInflater.from(context);
+    }
+
+    @Override
+    public int getCount() {
+        return images.size();
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        container.removeView((View) object);
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        return view.equals(object);
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup view, int position) {
+        View myImageLayout = inflater.inflate(R.layout.slide, view, false);
+        ImageView myImage = (ImageView) myImageLayout
+                .findViewById(R.id.image);
+        myImage.setImageResource(images.get(position));
+        view.addView(myImageLayout, 0);
+        return myImageLayout;
+    }
+}
